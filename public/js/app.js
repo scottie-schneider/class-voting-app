7b12401545ecdf78ea280db254b2f4140407a890
Scottie Schneider <scott@laclave.co>
2017-09-23 14:09:26 -0500

    It's alive! 😺

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,33 @@ class ProductList extends React.Component {
     this.state = {
       products: [],
     };
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId){
-    console.log(`${productId} was upvoted`);
+  handleProductUpVote(productId) {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
   }
+
   render() {
-    const products = Seed.products.sort((a,b) => (
+    const products = this.state.products.sort((a,b) => (
       b.votes - a.votes
     ))
-    const productComponents = Seed.products.map((product) => (
+    const productComponents = products.map((product) => (
       <Product
         key={'product-' + product.id}
         id={product.id}
