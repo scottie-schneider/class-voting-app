527619ea7f4a4a76abc1f2b40ae3004b1a82b1c2
Scottie Schneider <scott@laclave.co>
2017-09-23 13:23:42 -0500

    Adding State 🐵 💃

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,19 @@
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
+  handleProductUpVote(productId){
+    console.log(`${productId} was upvoted`);
+  }
   render() {
     const products = Seed.products.sort((a,b) => (
       b.votes - a.votes
@@ -13,6 +28,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ));
     return (
@@ -24,6 +40,14 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
   render() {
     return (
       <div className='item'>
@@ -32,8 +56,8 @@ class Product extends React.Component {
         </div>
         <div className='middle aligned content'>
           <div className='header'>
-            <a>
-              <i className='large caret up icon' />
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon'/>
             </a>
             {this.props.votes}
           </div>
